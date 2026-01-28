8960219f48805a042558d975f66b8bc242e355fa
Ina Backes <inabackes@live.de>
2026-01-28 18:55:55 +0100

    add shapiro wilk test

--- analysis/hilfen_analyse_gesamt.py
+++ analysis/hilfen_analyse_gesamt.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import shapiro
 from scipy import stats
 
 from helper.functions import summarize, read_nrw_map, find_outliers_iqr, top_bottom
@@ -13,9 +14,20 @@ df = pd.read_csv("../data/processed/master_2024.csv", sep=",", encoding="UTF-8")
 x = df["35a Hilfen pro 10000"]
 print(summarize(x))
 
-#top_bottom(df, "35a Hilfen pro 10000", n=5)
+#Normalverteilungstest mit Shapiro-Wilk
+stat, p = shapiro(x)
+
+print(f"H0: Die 35a-Quote ist normalverteilt.")
+print(f"Shapiro-Wilk-Statistik: {stat:.4f}")
+print(f"p-Wert: {p:.4f}")
+
+if p < 0.05:
+    print("Keine Normalverteilung (H0 wird verworfen).")
+else:
+    print("Normalverteilung kann nicht verworfen werden (H0 bleibt bestehen).")
 
 
+#visuelle Überprüfung mit KDE und QQ-Plot
 x_np = x.to_numpy()
 
 plt.figure()
@@ -27,7 +39,7 @@ mu = x_np.mean()
 sigma = x_np.std(ddof=1) # Degrees fo Freedom n-1 da Strichprobe, nicht Grundgesamtheit
 plt.plot(xx, stats.norm.pdf(xx, mu, sigma))
 
-# bandwith bei stardard gelassen, o.5 liefert ähnlcihe ergebnisse, höhere werte drücken die
+# Bandwith bei standard gelassen, 0.5 liefert ähnliche Ergebnisse, höhere Werte drücken die Kurve
 kde = stats.gaussian_kde(x_np)
 plt.plot(xx, kde(xx), color="red")
 
@@ -46,25 +58,24 @@ plt.show()
 
 plt.figure(figsize=(12,6))
 sns.barplot(data=df.sort_values('35a Hilfen pro 10000', ascending=False),
-            x='Name', y='35a Hilfen pro 10000')
-plt.xticks(rotation=90)
+            y='Name', x='35a Hilfen pro 10000')
 plt.title("In Anspruch genommene 35a Hilfen nach Kreis/Kreisfreier Stadt, auf je 10.000 Kinder")
 plt.show()
 
 mask = find_outliers_iqr(x)
-print(df.loc[mask, ["Name", "35a Hilfen pro 10000"]])
+print("Ausreißer: ", df.loc[mask, ["Name", "35a Hilfen pro 10000"]])
 
-# Visualisierung
+# Choroplethenkarte generieren
 nrw = read_nrw_map(df)
+
+### KI-generiert
 fig, ax = plt.subplots(1, 1, figsize=(18, 21))
 nrw.plot(column="35a Hilfen pro 10000", ax=ax, legend=True, cmap="OrRd", edgecolor="black")
 
 for idx, row in nrw.iterrows():
-    if row['geometry'].geom_type == 'Polygon':
-        x, y = row['geometry'].centroid.x, row['geometry'].centroid.y
-    else:  # MultiPolygon
-        x, y = row['geometry'].centroid.x, row['geometry'].centroid.y
+    x, y = row['geometry'].centroid.x, row['geometry'].centroid.y
     ax.text(x, y, row['GN'], fontsize=8, ha='center', va='center')
 
 ax.set_title("In Anspruch genommene 35a Hilfen in NRW auf je 10.000 junge Menschen")
 plt.show()
+####
