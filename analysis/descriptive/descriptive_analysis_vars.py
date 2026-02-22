a8b8cba25b2b1be5738d05e1f335937f7718915c
Ina Backes <inabackes@live.de>
2026-02-22 19:53:19 +0100

    add vif-checks in descriptive_analysis_vars.py

--- analysis/descriptive/descriptive_analysis_vars.py
+++ analysis/descriptive/descriptive_analysis_vars.py
@@ -1,31 +1,81 @@
-import pandas as pd
-from utils.descriptive_utils import basic_descriptive_analysis, descriptive_analysis_by_group, clean_outlier
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+from utils.descriptive_utils import basic_descriptive_analysis, descriptive_analysis_by_group, find_outlier
+from utils.commons import *
 
 # Einlesen
-df = pd.read_csv("../../data/processed/master_2024.csv", sep=",", encoding="UTF-8")
+df = pd.read_csv("data/processed/master_2024.csv", sep=",", encoding="UTF-8")
+
+variables = (
+    MIGRATION,
+    ABITUR,
+    SGBII,
+    KINDERANTEIL,
+    KINDERARZTDICHTE,
+    ERZ_HILFEN,
+    BEV_DICHTE
+)
 
 
-abi = "Abiturquote"
-sgbii ="SGB II-Quote"
-kinderarzt="Kinderarztdichte"
-kinderanteil = "Kinderanteil"
-dichte ="Bevölkerungsdichte"
-migration = "Migrationsanteil"
-erz_Hilfen = "erz. Hilfen pro 10000"
+def vif(dataframe, vars: tuple):
+    # Varianzinflationfaktor
+    X = dataframe[[*vars]].dropna()
+    X = sm.add_constant(X)
+
+    vif_data = pd.DataFrame()
+    vif_data["Variable"] = X.columns
+    vif_data["VIF"] = [
+        variance_inflation_factor(X.values, i)
+        for i in range(X.shape[1])
+    ]
+
+    vif_data = vif_data[vif_data["Variable"] != "const"]
+    vif_data = vif_data.sort_values("VIF", ascending=False)
+    vif_data.to_csv(result_path("multicollinearity") / "VIF.csv", index=False)
 
 
-variables= (kinderanteil, abi, dichte, sgbii, kinderarzt, migration, erz_Hilfen)
+def correlations(df: pd.DataFrame, vars: tuple):
+    corr = df[[*vars]].corr(method="spearman")
 
-df_cut = df[df["Name"] != "Aachen"]
-df_clean = df.copy()
+    # Nur obere Dreiecksmatrix (ohne Diagonale)
+    mask = np.triu(np.ones_like(corr, dtype=bool), k=1)
+    corr_pairs = corr.where(mask)
 
+    corr_pairs = (
+        corr_pairs
+        .stack()
+        .reset_index()
+    )
+    corr_pairs.columns = ["Variable_1", "Variable_2", "r"]
+
+    filtered = corr_pairs[abs(corr_pairs["r"]) > 0.5]
+
+    filtered.sort_values("r", ascending=False)
+    filtered.to_csv(result_path("multicollinearity") / "Korrelationen.csv", index=False)
+
+
+
+# Multikollinearität und Korrelationen prüfen
+vif(df, variables)
+# Bevölkerungsdichte mit Faktor 5,9 wird entfernt
+
+vars_new = list(variables)
+vars_new.remove(BEV_DICHTE)
+variables = tuple(vars_new)
+
+correlations(df, variables)
+
+# Hohe Korrelation bei SGB II Quote und Migrationsanteil, später in der Regression bedenken.
+
+# Deskriptive Analyse
 for var in variables:
-    basic_descriptive_analysis(df, var, shapiro=False)
-    descriptive_analysis_by_group(df_cut, var, "Kreisstrukturtyp", shapiro=False)
-    descriptive_analysis_by_group(df_cut, var, "Gebietskörperschaft", shapiro=False)
+    basic_descriptive_analysis(df, var)
+    descriptive_analysis_by_group(df[df["Name"] != "Aachen"], var, GEBIETSKOERPERSCHAFT)
+    out = find_outlier(df, var)
 
 
-df_cleaned = clean_outlier(df, kinderarzt)
-df_cleaned.rename(columns={kinderarzt: f"{kinderarzt}_cleaned"}, inplace=True)
 
-basic_descriptive_analysis(df_cleaned, f"{kinderarzt}_cleaned", shapiro=False)
+
+
