74b29319c5e7f59900bba67bbc7bd063ba5d20c5
Ina Backes <inabackes@live.de>
2026-03-24 00:29:52 +0100

    refactoring

--- analysis/inferential/35a_kreisstruktur_check.py
+++ analysis/inferential/35a_kreisstruktur_check.py
@@ -1,17 +1,17 @@
 import sys
 
-from matplotlib import pyplot as plt
 from scipy import stats
 from utils.commons import *
 from utils.descriptive_utils import plot
-from utils.inferential_utils import shapiro_wilk
+import matplotlib.pyplot as plt
 
+from utils.inferential_utils import shapiro_wilk
 
 df = pd.read_csv("data/processed/master_2024.csv", sep=",", encoding="UTF-8")
 df_cut = df[df["Name"] != "Aachen"]
 
 
-# Überprüfung von Varianzhomogenität und Normalverteilung
+# Überprüfung der Normalverteilung
 df_gr_kreis = df[df[KREISSTRUKTUR] == "Großer Kreis"].copy()
 df_stadt = df[df[KREISSTRUKTUR] == "Kreisfreie Stadt"].copy()
 df_kl_kreis = df[df[KREISSTRUKTUR] == "Kleiner Kreis"].copy()
@@ -23,18 +23,12 @@ fig.savefig(result_path("inferential/kreisstruktur_effekt")/"qqplots.png", dpi=3
 plt.close(fig)
 
 sh = shapiro_wilk(df_cut, HILFEN_35A, group_col=KREISSTRUKTUR)
-center="mean"
-lev_stat, lev_p = stats.levene(*groups, center="mean")
 
-with open("results/inferential/kreisstruktur_effekt/levene_shapiro.txt", "w", encoding="utf-8") as f:
+with open(result_path("inferential/kreisstruktur_effekt")/"shapiro.txt", "w", encoding="utf-8") as f:
     sys.stdout = f
     print(sh)
-    print("___________")
-    print(f"Levene-Statistik mit center={center}: ", round(lev_stat, 3))
-    print("Levene p-Wert: ", round(lev_p, 4))
 
-# Einfaktorielle Anova zur Überprüfung der Varianzgleichheit
-f_stat, p_value = stats.f_oneway(*groups)
+f_stat, p_value = stats.f_oneway(*groups, equal_var=False)
 
 with open("results/inferential/kreisstruktur_effekt/ANOVA.txt", "w", encoding="utf-8") as f:
     sys.stdout = f
